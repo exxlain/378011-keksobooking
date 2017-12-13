3aa9478248158b31ef12aa50cb5a74d82d01d21d
Lana Kostina <exxlain@gmail.com>
2017-12-14 00:38:46 +0700

    максимум подвижности

--- js/map.js
+++ js/map.js
@@ -8,9 +8,11 @@
     var mapWindow = document.querySelector('.map');
     var mapFiltersElement = mapWindow.querySelector('.map__filters-container');
     var noticeForm = document.querySelector('.notice__form');
+    var inputAddress = noticeForm.querySelector('#address');
     var mainPin = mapWindow.querySelector('.map__pin--main');
     var activeMapPin = null;
     var allFieldsets = noticeForm.querySelectorAll('fieldset');
+    var pinsOverlay = document.querySelector('.map__pinsoverlay');
 
     /*навешивает обработчик мыши на основной пин*/
     mainPin.addEventListener('mouseup', function() {
@@ -60,7 +62,6 @@
         document.addEventListener('keydown', function(evt) {
             onPopupEscPress(evt, document.querySelector('.popup'))
         });
-
     };
     /*активирует пин и вызывает попап*/
     var onPinMouseup = function (evt, obj) {
@@ -73,8 +74,6 @@
         activeMapPin.classList.add('map__pin--active');
         popupOpen(obj);
     };
-
-
     /*закрывает попап*/
     var popupClose = function (currentOffer) {
         mapWindow.querySelector('.map__pin--active').classList.remove('map__pin--active');
@@ -101,4 +100,63 @@
             popupClose(currentOffer);
         }
     };
+
+/* перемещение главного пина*/
+/*ограничения перемещения главного пина по высоте*/
+var minY = 100;
+var maxY = 500;
+/*размер хвостика главного пина*/
+var MAIN_TAIL_HEIGHT = 22;
+
+/* Функция для определения координат острого кончика главного пина*/
+var getCoordinates = function (element, container) {
+    var elementBox = element.getBoundingClientRect();
+    var overlayBox = container.getBoundingClientRect();
+    return Math.round((elementBox.left - overlayBox.left + elementBox.width / 2)) + ', ' + Math.round((elementBox.bottom + pageYOffset + MAIN_TAIL_HEIGHT));
+};
+
+/*внесение координат в поле адрес*/
+var getAddress = function () {
+    inputAddress.value = getCoordinates(mainPin, pinsOverlay);
+};
+
+/*  функция перемещения главного пина*/
+var onMainPinMousedown = function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+        x: evt.clientX,
+        y: evt.clientY
+    };
+    var onMouseMove = function(moveEvt) {
+        moveEvt.preventDefault();
+
+        var shift = {
+            x: startCoords.x - moveEvt.clientX,
+            y: startCoords.y - moveEvt.clientY
+        };
+
+        startCoords = {
+            x: moveEvt.clientX,
+            y: moveEvt.clientY
+        };
+        mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
+        if ((mainPin.offsetTop - shift.y) >=
+            (minY) && (mainPin.offsetTop - shift.y) <= (maxY)) {
+            mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
+        }
+    };
+    var onMouseUp = function (upEvt) {
+        upEvt.preventDefault();
+        getAddress();
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+};
+
+/*перемещение главного пина*/
+mainPin.addEventListener('mousedown', onMainPinMousedown);
 })();
